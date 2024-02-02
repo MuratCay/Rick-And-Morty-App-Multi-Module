c4fa98730d2d69197900818d6d57a2de41f038e2
Murat Çay <muratthebestof@gmail.com>
2024-02-03 01:04:32 +0300

    - GlideModule.kt yorum eklendi.

--- core/data/src/main/java/com/core/data/remote/di/GlideModule.kt
+++ core/data/src/main/java/com/core/data/remote/di/GlideModule.kt
@@ -2,6 +2,7 @@ package com.core.data.remote.di
 
 import android.content.Context
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.muratcay.rick_and_morty_app.R
 import dagger.Module
@@ -18,9 +19,11 @@ object GlideModule {
     @Singleton
     @Provides
     fun injectGlide(@ApplicationContext context: Context) =
-        Glide.with(context)
-            .setDefaultRequestOptions(
-                RequestOptions()
-                .placeholder(R.drawable.loading_img)
-                .error(R.drawable.ic_running_with_errors))
+        Glide.with(context).setDefaultRequestOptions(
+                RequestOptions().placeholder(R.drawable.loading_img)
+                    .error(R.drawable.ic_running_with_errors)
+                    .diskCacheStrategy(DiskCacheStrategy.DATA)
+            )
 }
+
+// diskCacheStrategy(DiskCacheStrategy.DATA)  Görüntü verilerini önbelleğe almak için kullanılır(Dönüştürülen görüntüyü almaz).
