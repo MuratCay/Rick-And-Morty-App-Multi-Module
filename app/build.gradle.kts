64cdbfb93ea15cd5ae093a79205f21e83e1da2b8
Murat Çay <muratthebestof@gmail.com>
2024-02-02 23:24:10 +0300

    - Timber eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,17 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
         }
     }
     compileOptions {
@@ -54,6 +59,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.timber)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/muratcay/rick_and_morty_app/RickAndMortyApplication.kt
+++ app/src/main/java/com/muratcay/rick_and_morty_app/RickAndMortyApplication.kt
@@ -1,7 +1,21 @@
 package com.muratcay.rick_and_morty_app
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class RickAndMortyApplication : Application()
+class RickAndMortyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (isDebuggable()) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private fun isDebuggable(): Boolean {
+        return (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
+    }
+}
