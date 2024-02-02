d56609989c5e9c0f45dcf6fe5e507beeb164d5e4
Murat Çay <muratthebestof@gmail.com>
2024-02-02 23:27:59 +0300

    - Environments.kt eklendi

---
+++ core/common/src/main/java/com/core/common/Environments.kt
@@ -0,0 +1,10 @@
+package com.core.common
+
+object Environments {
+    object Release {
+        const val appId = "com.muratcay.rick_and_morty_app"
+        const val appVersionCode = 1
+        const val appVersionName = "1.0"
+        const val baseUrl = "https://rickandmortyapi.com/api/"
+    }
+}
